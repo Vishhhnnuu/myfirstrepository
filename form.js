3c206cd1e3e01ddd12ecff5729fd44bc49608f56
Vishhhnnuu <guduguntlavishnu@gmail.com>
2025-06-30 15:37:47 +0530

    day 11

---
+++ form.js
@@ -0,0 +1,61 @@
+ let a = document.createElement("h1");
+        a.innerHTML = "Student Form";
+        document.body.appendChild(a);
+
+  
+   let b = document.createElement("label");
+    b.textContent="Name:";
+    document.body.appendChild(b);
+    b = document.createElement("input");
+    b.placeholder="enter your name";
+    document.body.appendChild(b);
+    br = document.createElement("br");
+    document.body.appendChild(br)
+    br = document.createElement("br");
+    document.body.appendChild(br)
+    
+    
+  
+  c = document.createElement("label");
+  c.textContent="Age:";
+  document.body.appendChild(c);
+  c = document.createElement("input");
+  c.placeholder="enter your age"; 
+  document.body.appendChild(c);
+  br = document.createElement("br");
+  document.body.appendChild(br)
+  br = document.createElement("br");
+  document.body.appendChild(br)
+  
+  
+         
+
+  d = document.createElement("label");
+  d.textContent="Email:";
+  document.body.appendChild(d);
+  d = document.createElement("input");
+  d.placeholder="enter your eamil";
+  document.body.appendChild(d);
+  br = document.createElement("br");
+  document.body.appendChild(br)
+  br = document.createElement("br");
+  document.body.appendChild(br)
+  
+
+
+  e = document.createElement("label");
+  e.textContent="Number:";
+  document.body.appendChild(e);
+  e= document.createElement("input");
+  e.placeholder="enter your number";
+  document.body.appendChild(e);
+  br = document.createElement("br");
+  document.body.appendChild(br)
+  br = document.createElement("br");
+  document.body.appendChild(br)
+
+  f = document.createElement("button");
+  f.textContent="submit";
+  document.body.appendChild(f);
+
+         
